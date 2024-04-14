74448eb0ca1acb4d8e6b13f2016bb7c4450ed3d4
Christopher Meddings <c.m.meddings@gmail.com>
2024-04-14 13:48:32 +0000

    Add views to about app and updated modal

--- about/urls.py
+++ about/urls.py
@@ -14,10 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('about/', include("about.urls"), name='about-urls'),
-    path('admin/', admin.site.urls),
+    path('', views.header_content, name='header'),
 ]
--- about/views.py
+++ about/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import About, Header
 
 # Create your views here.
 
-def about_me(request):
-    return HttpResponse("This would be the about page")
+def header_content(request):
+    header = Header.objects.all().order_by("updated_on").first()
+
+    return redner(
+        request,
+        "index.html",
+        {
+            "title": title,
+            "sub_title": sub_title,
+        },
+    )
