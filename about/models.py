6304906b42cba4323a2d0453a93d51f30a9be843
Christopher Meddings <c.m.meddings@gmail.com>
2024-04-14 13:36:19 +0000

    Create new model for about page, added cloudinary and migrated

--- about/models.py
+++ about/models.py
@@ -1,14 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
+class Header(models.Model):
+    """
+    Stores a title and sub title for the landing page
+    This is editable
+    """
+    title = models.CharField(max_length=200, unique=True)
+    sub_title = models.TextField()
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
 class About(models.Model):
     """
-    Stores a single about me text
+    Stores content for display on the landing page that provides the user information 
+    about the owner and purpose of websote.
     """
-    title = models.CharField(max_length=100, unique=True)
+    title = models.CharField(max_length=200, unique=True)
     content = models.TextField()
     updated_on = models.DateTimeField(auto_now=True)
+    profile_image = CloudinaryField('image', default='placeholder')
 
     def __str__(self):
         return self.title
--- the_ox_box/settings.py
+++ the_ox_box/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-cmed01-portfoliomilesto-fvpzypb9e6t.ws-eu110.gitpod.io',
@@ -46,6 +46,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
+    'django.contrib.sites',
+    'cloudinary',
     'about',
 ]
 
