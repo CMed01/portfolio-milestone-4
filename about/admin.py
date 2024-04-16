561e6f0ff658c0a5e16930e7fc8546120cf7bbf3
Christopher Meddings <c.m.meddings@gmail.com>
2024-04-16 06:07:57 +0000

    Add summernote to admin model to improve admin UX

--- about/admin.py
+++ about/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import About, Header
 
 # Register your models here.
 
-admin.site.register(About)
 admin.site.register(Header)
 
+@admin.register(About)
+class AboutAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'updated_on')
+    search_fields = ['title', 'content']
+    list_filter = ('updated_on','status')
+    summernote_fields = ('content',)
