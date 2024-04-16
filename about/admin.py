1f38883781806c2bb36a288edf8421d8f180c65f
Christopher Meddings <c.m.meddings@gmail.com>
2024-04-16 06:12:02 +0000

    Amend admin.py for about app

--- about/admin.py
+++ about/admin.py
@@ -3,12 +3,11 @@ from django_summernote.admin import SummernoteModelAdmin
 from .models import About, Header
 
 # Register your models here.
-
-admin.site.register(Header)
-
 @admin.register(About)
 class AboutAdmin(SummernoteModelAdmin):
     list_display = ('title', 'updated_on')
     search_fields = ['title', 'content']
     list_filter = ('updated_on','status')
     summernote_fields = ('content',)
+
+admin.site.register(Header)
