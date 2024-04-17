6434e9271513541fd802730c31b4ac0c678fdc83
Christopher Meddings <c.m.meddings@gmail.com>
2024-04-17 09:15:40 +0000

    Updated base template to allow user to navigate between home and blog pages

--- blog/urls.py
+++ blog/urls.py
@@ -18,5 +18,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='blog-home'),
+    path('', views.PostList.as_view(), name='blog'),
 ]
