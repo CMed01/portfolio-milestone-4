8a2307c09d97b496c42dbe8add461e59b584f50d
Christopher Meddings <c.m.meddings@gmail.com>
2024-04-20 13:56:50 +0000

    Create modeal for Post Comment

--- blog/models.py
+++ blog/models.py
@@ -26,3 +26,14 @@ class Post(models.Model):
 
     def __str__(self):
         return f"{self.title} | written by {self.author}"
+
+class PostComment(models.Model):
+    post = models.ForeignKey(
+        Post,on_delete=models.CASCADE, related_name="comments"
+        )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter"
+        )
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
