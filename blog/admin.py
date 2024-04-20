5a5fd938039fab222e81c7f8a8dfddc98056d6c7
Christopher Meddings <c.m.meddings@gmail.com>
2024-04-20 14:02:00 +0000

    Update admin with new model in blog app

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, PostComment
 from django_summernote.admin import SummernoteModelAdmin
 # Register your models here.
 
@@ -10,3 +10,10 @@ class PostAdmin(SummernoteModelAdmin):
     list_filter = ('status','created_on',)
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
+
+@admin.register(PostComment) 
+class PostCommentAdmin(SummernoteModelAdmin):
+    list_display = ('post', 'author', 'approved', 'created_on')
+    search_fields = ['post','body']
+    list_filter = ('author','approved','created_on')
+    summernote_fields = ('body',)
