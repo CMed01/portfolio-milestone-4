5cc38da73e084fd3553420b8ecf3d370e9290157
Christopher Meddings <c.m.meddings@gmail.com>
2024-04-21 20:16:48 +0000

    Added links to about me page and update userprofile page

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.view_profile, name='profile'),
+    path('profile/<int:bio_id>',
+         views.edit_profile, name='profile_edit'),
 ]
--- userprofile/views.py
+++ userprofile/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from django.http import HttpResponseRedirect
 from .models import Profile, User
+from .forms import ProfileForm
 from django.contrib import messages
 
 # Create your views here.
@@ -10,23 +11,36 @@ def view_profile(request):
 
     queryset = Profile.objects.all()
     profile = get_object_or_404(queryset, author=request.user)
+    profile_form = ProfileForm()
     return render(
         request, 
         'profile.html',
         {
             'profile': profile,
+            'profile_form': profile_form,
             },
     )
 
-# def edit_profile(request):
-#     profile = request.user.profile
+def edit_profile(request):
+    profile = request.user.profile
 
-#     if request.method == 'POST':
-#         profile_form = ProfileForm(request.POST, instance=profile)
-#         if profile_form.is_valid():
-#             profile_form.save()
-#             return redirect('profile')
-#     else:
-#         form = ProfileForm(instance=profile)
+    if request.method == 'POST':
+        profile = get_object_or_404(Profile, pk=profile_id)
+        profile_form = ProfileForm(data=request.POST, instance=profile)
+        if profile_form.is_valid() and profile.author == request.user:
+            profile = profile_form.save()
+            messages.add_message(
+                request, 
+                messages.SUCCESS, 
+                'Profile updated'
+                )
+        else:
+            messages.add_message(
+            request, 
+            messages.ERROR, 
+            'Error updating profile!'
+            )
 
-#     return render(request, 'edit_profile.html', {'form': form})   
+    return HttpResponseRedirect(reverse('profile',args=[slug])) 
+    
+     
