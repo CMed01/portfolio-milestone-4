48d75bb4a2d9f59886354ab48307f0ac124bc748
Christopher Meddings <c.m.meddings@gmail.com>
2024-04-28 15:44:53 +0000

    Add function descritions to javascript code and format

--- static/js/blog_comments.js
+++ static/js/blog_comments.js
@@ -7,9 +7,15 @@ const deleteButtons = document.getElementsByClassName("btn-delete");
 const deleteConfirm = document.getElementById("deleteConfirm");
 
 /**
+ * Initializes edit functionality for the provided edit buttons.
  * 
+ * For each button in the `editButtons` array:
+ * - On click, associated comment ID is retrieved.
+ * - Content of the corresponding comment is stored.
+ * - Input/text area of `blogCommentText` populated with editable comment.
+ * - Subite button text amended to 'Update'
+ * - Form's action attribute set to the `edit_comment/{commentId}`.
  */
-
 for (let button of editButtons) {
     button.addEventListener("click", (e) => {
         let commentId = e.target.getAttribute("comment_id");
@@ -20,6 +26,16 @@ for (let button of editButtons) {
     })
 }
 
+/**
+ * Initializes delete functionality for the provided delete buttons.
+ * 
+ * For each button in the `deleteButtons` array:
+ * - On click, associated comment ID is retrieved.
+ * - href for 'deleteConfirm' updated to the correct endpoint -
+ *  `delete_comment/${commentId}`
+ * - Delete confirmation modal ('deleteModal') is displayed 
+ * for the user to confirm
+ */
 for (let button of deleteButtons) {
     button.addEventListener("click", (e) => {
         let commentId = e.target.getAttribute("comment_id");
--- static/js/workout_comments.js
+++ static/js/workout_comments.js
@@ -3,9 +3,15 @@ const deleteButtons = document.getElementsByClassName("btn-delete");
 const deleteConfirm = document.getElementById("deleteConfirm");
 
 /**
+ * Initializes delete functionality for the provided delete buttons.
  * 
+ * For each button in the `deleteButtons` array:
+ * - On click, associated comment ID is retrieved.
+ * - href for 'deleteConfirm' updated to the correct endpoint -
+ *  `delete_comment/${commentId}`
+ * - Delete confirmation modal ('deleteModal') is displayed 
+ * for the user to confirm
  */
-
 for (let button of deleteButtons) {
     button.addEventListener("click", (e) => {
         let workoutcommentId = e.target.getAttribute("comment_id");
